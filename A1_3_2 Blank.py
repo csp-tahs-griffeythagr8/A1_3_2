b69c5f34beb095fd31283a7acf707971edeca8da
RW3110 <RW3110@k12.sd.us>
2019-11-12 08:58:44 -0600

    Rename Doc
    I renamed the document to make it my own. I also indicated my intentions to make sure I check the ReadMe file.

--- A1_3_2 Blank.py
+++
@@ -1,6 +0,0 @@
-"""
-A1_3_2 #Replace With Your Last Name.py
-Programmer:
-"""
-
-#Place Your Code Below
---
+++ A1_3_2 Westhoff.py
@@ -0,0 +1,8 @@
+"""
+A1_3_2 Westhoff.py
+Programmer: Reece Westhoff
+"""
+
+#Place Your Code Below
+#I will read the repository ReadMe
+#I will update document with my own code
