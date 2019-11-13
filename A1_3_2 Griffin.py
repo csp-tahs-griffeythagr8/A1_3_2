d929c09eb2714fc83552520672f65b49ede4d833
griffeyTheGr8 <42072301+griffeyTheGr8@users.noreply.github.com>
2019-11-13 08:11:26 -0600

    Update A1_3_2 Griffin.py
    Added necessary variables. Created the game function. Called the function to ensure its working correctly. Added documentation as well.

--- A1_3_2 Griffin.py
+++ A1_3_2 Griffin.py
@@ -2,7 +2,15 @@
 A1_3_2 Griffin.py
 Programmer:
 """
+#Creating Variables
+#Create a variable 'guess'. This variable is to be assigned a value from 1-100.
 
-#Place Your Code Below
-#I will read the repository ReadMe
-#I will update document with my own code!
+guess = 52 #guess variable
+
+#Creating Functions
+
+def hiLo_game(guess): #hiLo_game function w/ arg guess
+    change = guess - 3 #change variable holds expression 'guess-3'
+    print(change)
+
+hiLo_game(guess)
