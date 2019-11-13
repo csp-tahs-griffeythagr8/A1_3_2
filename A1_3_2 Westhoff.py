2089af66c003db7437fdb5c43911024a23ac0409
RW3110 <RW3110@k12.sd.us>
2019-11-13 08:33:40 -0600

    Update A1_3_2 Westhoff.py

--- A1_3_2 Westhoff.py
+++ A1_3_2 Westhoff.py
@@ -6,3 +6,8 @@ Programmer: Reece Westhoff
 #Place Your Code Below
 #I will read the repository ReadMe
 #I will update document with my own code
+import random
+guess = random.randint(1,100)
+def hiLo_game(guess):
+    change = guess + 36
+    print(change)
