0fa67675ec0ee7832d61fcb3e72fd59df1c674f0
es3495 <57448571+es3495@users.noreply.github.com>
2019-11-13 09:01:09 -0600

    Got it working
    I changed the program so it now has no bugs and it works great.

--- A1_3_2 Stainbrook.py
+++ A1_3_2 Stainbrook.py
@@ -1,6 +1,11 @@
 """
 A1_3_2 #Replace With Your Last Name.py
-Programmer:
+Programmer: Ethan S
 """
 
 #Place Your Code Below
+import random
+guess = random.randint(1,100)
+def hiLo_game(guess):
+    change = guess + 87
+    print(change)
