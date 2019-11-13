255dffaad717c434c5a0cf2d4eaeebb32fa27cd2
cm3365 <57448218+cm3365@users.noreply.github.com>
2019-11-13 09:02:31 -0600

    Update A1_3_2 Mckinney_Erickson.py

--- A1_3_2 Mckinney_Erickson.py
+++ A1_3_2 Mckinney_Erickson.py
@@ -1,8 +1,9 @@
-"""
-A1_3_2 Mckinney/Erickson.py
-Programmer:
-"""
-
-#Place Your Code Below
-#I will read the repository ReadMe
-# I will update document with my own code
+'''
+1.3.2
+Connor Mckinney
+'''
+import random
+guess=random.randint(1,100)
+def hiLo_game(guess):
+    change=guess+12
+    print(change)
