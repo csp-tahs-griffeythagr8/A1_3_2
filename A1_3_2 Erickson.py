a1ea990afcfb91a17ab48a26288c7bf1b1c2be52
we3008 <57448515+we3008@users.noreply.github.com>
2019-11-19 08:32:41 -0600

    WE feature
    WE feature

--- A1_3_2 Erickson.py
+++ A1_3_2 Erickson.py
@@ -2,5 +2,14 @@
 A1_3_2 Erickson.py
 Programmer:Wyatt Erickson
 """
+import random
+guess = 12
 
-#Place Your Code Below
+def hiLo_game(guess):
+    rand_num = random.randint(1,100)
+    if guess == rand_num:
+        print('congrats you found the correct number')
+    elif guess < rand_num:
+        print('this is too low')
+    else guess > rand_num:
+        print('this is too high')
