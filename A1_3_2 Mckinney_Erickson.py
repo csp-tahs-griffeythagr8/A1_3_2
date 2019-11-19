44ae2d5cd23a134f1bec9915a7977b5f4a47165a
griffeyTheGr8 <42072301+griffeyTheGr8@users.noreply.github.com>
2019-11-19 09:16:43 -0600

    Grading update 1
    Check the docstring at the bottom for things to work on.

--- A1_3_2 Mckinney_Erickson.py
+++ A1_3_2 Mckinney_Erickson.py
@@ -7,3 +7,20 @@ guess=random.randint(1,100)
 def hiLo_game(guess):
     change=guess+12
     print(change)
+    
+"""
+Spacing: 
+1. Make sure to provide space after each line to improve readability. 
+2. Space added between variable, assignment operator, and value/expression for guess.
+3. Space added between variable, assignment operator, and value/expression for change.
+You DO NOT need to add space between lines in your function. 
+
+Guess Variable:
+This is the user generated guess - we are placing our own value for testing purposes, thus while it is okay to have a random number now understand that it will be replaced by user input in the future. 
+
+Documentation:
+1. Need to use triple double quotes \"\"\" for docstrings.
+2. Need to add EOL comments using a '#' to clarify purpose of main items/elements i.e. guess, hiLo_game,  change.
+
+Not that it is necessary, however if you call the function after defining it from within the script it will run automatically once script is run. 
+"""
